b9d8d0dc78a1ce100271b11ee0d7801fd8321e10
Alex <alexandersuglio@gmail.com>
2017-03-23 00:00:05 -0400

    added new changes to the project

---
+++ trivia.js
@@ -0,0 +1,65 @@
+// $('body').click(function(){
+
+// alert('hey');
+
+// });
+
+// var q1 = 15,
+// q2= 23,
+// q3= 17,
+// q4= 9;
+// var userClick1 = ;
+
+// var A = 1,
+// B = 2,
+// C = 3,
+// D = 4;
+
+
+// var answerKey = ['1', '2', '3', '4'];
+
+// if ( A == answerKey[0]){
+// 	console.log("hey");
+// }
+
+// else {
+// 	console.log("wrong");
+// };
+
+
+var right = 0,
+    wrong = 0;
+
+var userSelection = null;
+
+var answerKey = ["15", "UF", "Gator", "Jim McElwain", "Gainesville", ];
+// console.log(answerKey[3]);
+
+var answer1 = $(".answerA").click(function(){
+	console.log(answerKey[0]);
+});
+
+
+
+
+
+
+
+// var question1Answers = ["15", "23", "17", "9"];
+
+// if (userSelection > question1Answers[0]) {
+//     console.log("right");
+// } else {
+//     console.log("wrong");
+// };
+
+
+
+
+
+
+
+
+// console.log(answerKey[0]);
+
+// console.log();
