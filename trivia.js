9e1e9eb316e867c246d9d3cefd79b78be5bdaca1
Alex <alexandersuglio@gmail.com>
2017-03-25 01:06:49 -0400

    removed seperate 'Begin' and 'Results' page, joined into Index

--- trivia.js
+++ trivia.js
@@ -1,195 +1,101 @@
-var right = 0,
-    wrong = 0,
-    userCounter= 0;
+// var right = [],
+//     wrong = [],
+//     userCounter = 0;
+
+var correctTally = 0,
+    wrongTally = 0;
 
 
-// function question1() {
+// $("#quizBody").hide();
 
-//     $("#answerA").one("click", function() {
-//         right++;
-
-//         // console.log("right");
-//         // console.log(right);
-
-//     });
-
-//     $("#answerB, #answerC, #answerD").one("click", function() {
-//         wrong++;
-//         // console.log("wrong");
-//         // console.log(wrong);
-//     });
+// $(".beginButton").click(function(e){
+// e.preventDefault();
+// $("#quizBody").show();
+// });
 
 
-// };
+$("#quizBody").hide();
 
-// question1();
+$(".beginButton").click(function(e) {
+    e.preventDefault();
+    $("#quizBody").show();
+    $(".beginButton").hide();
+});
 
-// function question2() {
 
-//     $("#answerC_ii").one("click", function() {
-//         right++;
 
-//         // console.log("right");
-//     });
+$("#result").hide();
 
-//     $("#answerA_ii, #answerB_ii, #answerD_ii").one("click", function() {
+$(".submitButton").click(function(e) {
+    e.preventDefault();
+    $("#result").show();
+    $(".submitButton").hide();
+});
 
-//         // console.log("wrong");
-//         wrong++;
-//     });
 
-// };
-
-// question2();
-
-// function question3() {
-
-//     $("#answerD_iii").one("click", function() {
-//         right++;
-
-//         // console.log("right");
-//     });
-
-//     $("#answerA_iii, #answerB_iii, #answerC_iii").one("click", function() {
-//         wrong++;
-//         // console.log("wrong");
-//     });
-
-// };
-
-// question3();
-
-// function question4() {
-
-//     $("#answerC_iv").one("click", function() {
-//         right++;
-
-//         // console.log("right");
-//     });
-
-//     $("#answerA_iv, #answerB_iv, #answerD_iv").one("click", function() {
-//         wrong++;
-//         // console.log("wrong");
-//     });
-
-// };
-
-// question4();
 
 $("#answerA").one("click", function() {
-        right++;
-       var correctTally = userCounter + right;
-       console.log(correctTally);
+    correctTally++;
 
-        // console.log("right");
-        // console.log(right);
 
-    });
+    // console.log(correctTally);
+    $("#correctFinal").text(correctTally);
 
-    $("#answerB, #answerC, #answerD").one("click", function() {
-        wrong++;
-        var wrongTally = userCounter + wrong;
-        console.log(wrongTally);
-        // console.log("wrong");
-        // console.log(wrong);
-    });
+
+});
+
+$("#answerB, #answerC, #answerD").one("click", function() {
+    wrongTally++;
+    $("#wrongFinal").text(wrongTally);
+
+});
 
 
 $("#answerC_ii").one("click", function() {
+    correctTally++;
 
-        right++;
-          var correctTally = userCounter + right;
-       console.log(correctTally);
+    $("#correctFinal").text(correctTally);
 
+});
 
-        // console.log("right");
-    });
-
-    $("#answerA_ii, #answerB_ii, #answerD_ii").one("click", function() {
-
-        // console.log("wrong");
-        wrong++;
-         var wrongTally = userCounter + wrong;
-        console.log(wrongTally);
-    });
+$("#answerA_ii, #answerB_ii, #answerD_ii").one("click", function() {
+    wrongTally++;
+    $("#wrongFinal").text(wrongTally);
+});
 
 
 $("#answerD_iii").one("click", function() {
-        right++;
-        var correctTally = userCounter + right;
-       console.log(correctTally);
+    correctTally++;
+    $("#correctFinal").text(correctTally);
 
+});
 
-        // console.log("right");
-    });
-
-    $("#answerA_iii, #answerB_iii, #answerC_iii").one("click", function() {
-        wrong++;
-            var wrongTally = userCounter + wrong;
-        console.log(wrongTally);
-        // console.log("wrong");
-    });
+$("#answerA_iii, #answerB_iii, #answerC_iii").one("click", function() {
+    wrongTally++;
+    $("#wrongFinal").text(wrongTally);
+});
 
 $("#answerC_iv").one("click", function() {
-        right++;
-        var correctTally = userCounter + right;
-       console.log(correctTally);
+    correctTally++;
+    $("#correctFinal").text(correctTally);
+
+});
+
+$("#answerA_iv, #answerB_iv, #answerD_iv").one("click", function() {
+    wrongTally++;
+    $("#wrongFinal").text(wrongTally);
+
+});
+
+$("#answerB_v").one("click", function() {
+    correctTally++;
+    $("#correctFinal").text(correctTally);
 
 
-        // console.log("right");
-    });
+});
 
-    $("#answerA_iv, #answerB_iv, #answerD_iv").one("click", function() {
-        wrong++;
-            var wrongTally = userCounter + wrong;
-        console.log(wrongTally);
-        // console.log("wrong");
-    });
+$("#answerA_v, #answerC_v, #answerD_v").one("click", function() {
+    wrongTally++;
+    $("#wrongFinal").text(wrongTally);
 
- $("#answerB_v").one("click", function() {
-        right++;
-       var correctTally = userCounter + right;
-       console.log(correctTally);
-
-
-        // console.log(right);
-    });
-
-    $("#answerA_v, #answerC_v, #answerD_v").one("click", function() {
-        wrong++;
-            var wrongTally = userCounter + wrong;
-        console.log(wrongTally);
-        // console.log("wrong");
-        // console.log(wrong);
-    });
-
-    // console.log(right);
-    // console.log(wrong);
-
-$("#rightFinal").text(correctTally);
-$("#wrongFinal").text(wrongTally);
-
-// function question5() {
-
-//     $("#answerB_v").one("click", function() {
-//         right++;
-
-//         // console.log(right);
-//     });
-
-//     $("#answerA_v, #answerC_v, #answerD_v").one("click", function() {
-//         wrong++;
-//         // console.log("wrong");
-//         // console.log(wrong);
-//     });
-
-// };
-
-// question5();
-
-// function total() {
-//     console.log(right);
-//     console.log(wrong);
-// };
-
-// total();
+});
