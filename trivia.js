fd2552202ec43326a8de735b03ec435bbcc625b1
Alex <alexandersuglio@gmail.com>
2017-03-28 15:47:15 -0400

    created a functioning timer

--- trivia.js
+++ trivia.js
@@ -1,18 +1,29 @@
-// var right = [],
-//     wrong = [],
-//     userCounter = 0;
+var correctTally,
+    wrongTally,
+    totalQuestions,
+    finalGrade;
 
-var correctTally = 0,
-    wrongTally = 0;
+var countSec = 50;
 
+$(".beginButton").one("click", function() {
 
-// $("#quizBody").hide();
+    var counterSec = setInterval(timer4sec, 1000);
 
-// $(".beginButton").click(function(e){
-// e.preventDefault();
-// $("#quizBody").show();
-// });
+    function timer4sec() {
 
+        countSec = countSec - 1;
+        if (countSec <= 0) {
+            alert("Out of time!");
+            location.reload();
+        }
+        document.getElementById("timerDataHereSec").innerHTML = countSec + " sec ";
+    };
+    if (
+        $(".submitButton").click(function() {
+            clearInterval(counterSec);
+        })
+    );
+});
 
 $("#quizBody").hide();
 
@@ -22,8 +33,6 @@ $(".beginButton").click(function(e) {
     $(".beginButton").hide();
 });
 
-
-
 $("#result").hide();
 
 $(".submitButton").click(function(e) {
@@ -32,70 +41,79 @@ $(".submitButton").click(function(e) {
     $(".submitButton").hide();
 });
 
+function quizQuestions() {
+
+    var correctTally = 0,
+        wrongTally = 0,
+        totalQuestions = 5;
 
 
-$("#answerA").one("click", function() {
-    correctTally++;
+    $("#answerA").one("click", function() {
+        correctTally++;
+        $("#correctFinal").text(correctTally);
+
+    });
+
+    $("#answerB, #answerC, #answerD").one("click", function() {
+        wrongTally++;
+        $("#wrongFinal").text(wrongTally);
+
+    });
 
 
-    // console.log(correctTally);
-    $("#correctFinal").text(correctTally);
+    $("#answerC_ii").one("click", function() {
+        correctTally++;
+
+        $("#correctFinal").text(correctTally);
+
+    });
+
+    $("#answerA_ii, #answerB_ii, #answerD_ii").one("click", function() {
+        wrongTally++;
+        $("#wrongFinal").text(wrongTally);
+    });
 
 
-});
+    $("#answerD_iii").one("click", function() {
+        correctTally++;
+        $("#correctFinal").text(correctTally);
 
-$("#answerB, #answerC, #answerD").one("click", function() {
-    wrongTally++;
-    $("#wrongFinal").text(wrongTally);
+    });
 
-});
+    $("#answerA_iii, #answerB_iii, #answerC_iii").one("click", function() {
+        wrongTally++;
+        $("#wrongFinal").text(wrongTally);
+    });
 
+    $("#answerC_iv").one("click", function() {
+        correctTally++;
+        $("#correctFinal").text(correctTally);
 
-$("#answerC_ii").one("click", function() {
-    correctTally++;
+    });
 
-    $("#correctFinal").text(correctTally);
+    $("#answerA_iv, #answerB_iv, #answerD_iv").one("click", function() {
+        wrongTally++;
+        $("#wrongFinal").text(wrongTally);
 
-});
+    });
 
-$("#answerA_ii, #answerB_ii, #answerD_ii").one("click", function() {
-    wrongTally++;
-    $("#wrongFinal").text(wrongTally);
-});
+    $("#answerB_v").one("click", function() {
+        correctTally++;
+        $("#correctFinal").text(correctTally);
+        finalGrade = correctTally / totalQuestions * 100;
+        $("#percentageShow").text("You know " + finalGrade + "% about Gator Football");
 
+    });
 
-$("#answerD_iii").one("click", function() {
-    correctTally++;
-    $("#correctFinal").text(correctTally);
-
-});
-
-$("#answerA_iii, #answerB_iii, #answerC_iii").one("click", function() {
-    wrongTally++;
-    $("#wrongFinal").text(wrongTally);
-});
-
-$("#answerC_iv").one("click", function() {
-    correctTally++;
-    $("#correctFinal").text(correctTally);
-
-});
-
-$("#answerA_iv, #answerB_iv, #answerD_iv").one("click", function() {
-    wrongTally++;
-    $("#wrongFinal").text(wrongTally);
-
-});
-
-$("#answerB_v").one("click", function() {
-    correctTally++;
-    $("#correctFinal").text(correctTally);
+    $("#answerA_v, #answerC_v, #answerD_v").one("click", function() {
+        wrongTally++;
+        $("#wrongFinal").text(wrongTally);
+        finalGrade = correctTally / totalQuestions * 100;
+        $("#percentageShow").text("You know " + finalGrade + "% about Gator Football");
+        // console.log(correctTally);
+    });
 
 
-});
+};
 
-$("#answerA_v, #answerC_v, #answerD_v").one("click", function() {
-    wrongTally++;
-    $("#wrongFinal").text(wrongTally);
-
-});
+quizQuestions();
